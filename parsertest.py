16294b2884e1bb51625b780182eca6810db4bb58
AmalieMoerck <amalie.moerck@gmail.com>
2026-09-24 11:09:44 +0200

    unittest 95%

--- parsertest.py
+++ parsertest.py
@@ -25,27 +25,28 @@ class TestStringMethods(unittest.TestCase):
         )
 
     def test_unquoted_field(self):
-        bad_line = 'Tobias,45,Århus"Danmark"'
+        line = 'Tobias,45,Århus"Danmark"'
 
         with self.assertRaises(ValueError):
-            line_parser(bad_line)
+            line_parser(line)
 
     def test_nestedquote_open(self):
-        bad_line = 'Tobias,45,"Århus""Danmark"'
+        line = 'Tobias,45,"Århus, ""Danmark""'
 
         with self.assertRaises(ValueError):
-            line_parser(bad_line)
+            line_parser(line)
 
 
     def test_line_parser_nested_qoutes(self):
-        line = 'Tobias,45,"Århus,""Danmark"",Jylland"'
+        line = 'Tobias,45,"Århus,""Danmark"", Jylland"'
   
         result = line_parser(line)
 
         self.assertEqual(
             result,
-            ['Tobias', '45', '"Århus,"Danmark", Jylland"']
+            ["Tobias", "45", 'Århus,"Danmark", Jylland']
         )
+
 
     def test_csv_parser(self):
         with open("testparser.csv", "w", encoding="utf-8") as file:
@@ -89,39 +90,71 @@ class TestStringMethods(unittest.TestCase):
                    
         self.assertEqual(result, expected)
 
-    #def test_wrong_number_fields(self):
-    #    with open("testnumberfields.csv", "w", encoding="utf-8") as file:
-    #        file.write(
-    #            "navn,alder,by\n"
-    #            "Tobias,45\n"
-    #            "Katrine,56,Vejle\n"
-    #        )
-    #    result, error = csv_parser("testnumberfields.csv")
+    def test_wrong_number_fields(self):
+        with open("testnumberoffields.csv", "w", encoding="utf-8") as file:
+            file.write(
+                "navn,alder,by\n"
+                "Tobias,45\n"
+                "Katrine,56,Vejle\n"
+            )
+        result, error = csv_parser("testnumberfields.csv")
 
-    #    expected = [
-    #        {'navn':'Katrine','alder':'56','by':'Vejle'},
-    #        {(1,'Tobias,45','wrong number of fields')}
-    #    ]
+        self.assertEqual(result, [
+            {"navn": "Katrine", "alder": "56", "by": "Vejle"}
+        ])
+        self.assertEqual(error, 
+                         [
+                             (2,"Tobias,45","wrong number of fields")
+                         ])
 
-    #    self.assertEqual(result, expected)
-    #    self.assertEqual(error, [])
+    def test_null_values(self):
+        with open("testnull.csv", "w", encoding="utf-8") as file:
+                   file.write(
+                        'navn,alder,by\n'
+                        'Tobias,,Aarhus\n'
+                        'Katrine,56,Vejle\n'
+                    )
+        result, error = csv_parser("testnull.csv")
+        expected = [{"navn": "Tobias", "alder": "NULL", "by": "Aarhus"},
+                    {"navn": "Katrine", "alder": "56", "by": "Vejle"}]
+        self.assertEqual(result, expected)
+        self.assertEqual(error, [])
+
+    def test_csv_parser_nested_qoutes(self):
+        with open("test1.csv", "w", encoding="utf-8") as file:
+           file.write(
+                'navn,alder,by\n'
+                'Tobias,45,"Aarhus,""Hej"""\n'
+                'Katrine,56,Vejle\n'
+            )
+        result, error = csv_parser("test1.csv")
+
+        expected = [
+            {'navn': 'Tobias', 'alder': '45', 'by': 'Aarhus,"Hej"'},
+            {'navn': 'Katrine', 'alder': '56', 'by': 'Vejle'}
+        ]
+
+        self.assertEqual(result, expected)
+        self.assertEqual(error, [])
+
+    def test_value_error(self):
+        with open("test_value_error.csv", "w", encoding="utf-8") as file:
+            file.write(
+                "navn,alder,by\n"
+                'Katrine,56,Vej"le\n'
+            )
+
+        result,error = csv_parser("test_value_error.csv")
+
+        self.assertEqual(result, [])
+
+        self.assertEqual(error,
+                         [
+                             (2,'Katrine,56,Vej"le',"Double quotes can not be in an unquoted field")
+                         ]
+                         )
 
 
-    #def test_csv_parser_esc_qoutes(self):
-    #    with open("test1.csv", "w", encoding="utf-8") as file:
-    #       file.write(
-    #            'navn,alder,by\n'
-    #            'Tobias,45,"Aarhus,""Hej"""\n'
-    #            'Katrine,56,Vejle\n'
-    #        )
-    #    result = csv_parser("test1.csv")
-
-    #    expected = [
-    #        {'navn': 'Tobias', 'alder': '45', 'by': 'Aarhus,"Hej"'},
-    #        {'navn': 'Katrine', 'alder': '56', 'by': 'Vejle'}
-    #    ]
-
-    #    self.assertEqual(result, expected)
    
 
 if __name__ == '__main__':
--- testcsv.py
+++
@@ -1,8 +0,0 @@
-import os
-import csv
-
-with open('employees.ascii.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        print(row['name'], row['email'])
-print (row)
